da51455c7f7c9d4b66acf5b62d1411e20a83f128
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-10-05 17:50:06 -0300

    create user entity and hook it up with user.module for it to be able to use it

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { UserModule } from './user/user.module';
 import { ReportModule } from './report/report.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './user/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
     UserModule,
