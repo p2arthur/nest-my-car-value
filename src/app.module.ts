4ec5ec82d3dba2d09e622be2157768bdcf992968
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-10-05 17:38:28 -0300

    add sqlite + sqlite connection to our app module

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { ReportModule } from './report/report.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule,
+    ReportModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { UserModule } from './user/user.module';
 import { ReportModule } from './report/report.module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create([UserModule, ReportModule]);
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
