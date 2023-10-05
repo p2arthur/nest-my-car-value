feba0b044ce9bdaf7b3ae2192ebccfdcf7a8f956
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-10-05 19:59:43 -0300

    add route for /auth/signup + create service to add signup users to the sqlite database

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,16 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UserService } from './users.service';
 
 @Controller('auth')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
+    const response = this.userService.create(body.email, body.password);
     return {
       stautsCode: 201,
-      body: JSON.stringify(
-        `User with email ${body.email} created successfully`,
-      ),
+      body: JSON.stringify(response),
     };
   }
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@nestjs/common';
-
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from './users.entity';
 @Injectable()
 export class UserService {
-  create(email: string, password: string) {}
+  constructor(@InjectRepository(Users) private repo: Repository<Users>) {}
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
 }
