ea974de672194a22f8a0997ee5fcbbd2847dffae
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-10-07 00:08:33 -0300

    add type annotations to our custom serialize decorator

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -8,16 +8,20 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToInstance } from 'class-transformer';
 
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+
 //Create a decorator to use our serialized interceptor with a dynamic dto
-export function Serialize(dto: any) {
+export function Serialize(dto: ClassConstructor) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
 export class SerializeInterceptor implements NestInterceptor {
-  constructor(private dto: any) {}
+  constructor(private dto: ClassConstructor) {}
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     return handler.handle().pipe(
-      map((data: any) => {
+      map((data: object) => {
         return plainToInstance(this.dto, data, {
           excludeExtraneousValues: true,
         });
