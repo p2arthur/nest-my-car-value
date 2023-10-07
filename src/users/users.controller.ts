6f2289631bde8d2590acc1d38139fe08b65376a8
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-10-06 23:59:06 -0300

    add serialized interceptor to all responses sent to requests made to /auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,39 +17,45 @@ import { UserService } from './users.service';
 import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
+import { User } from './users.entity';
+
+let user: User | User[];
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UserController {
   constructor(private usersService: UserService) {}
 
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
+  async createUser(@Body() body: CreateUserDto) {
+    user = await this.usersService.create(body.email, body.password);
+    return user;
   }
 
-  @Serialize(UserDto) //Custom decorator
   @Get('/:id')
   async findUser(@Param('id') id: string) {
-    const user = await this.usersService.findOne(id);
+    user = await this.usersService.findOne(id);
     if (!user) {
       throw new NotFoundException('user not found');
     }
     return user;
   }
 
-  @Serialize(UserDto) //Custom decorator
   @Get()
-  findAllUsers(@Query('email') email: string) {
-    return this.usersService.find(email);
+  async findAllUsers(@Query('email') email: string) {
+    user = await this.usersService.find(email);
+    return user;
   }
 
   @Delete('/:id')
-  removeUser(@Param('id') id: string) {
-    return this.usersService.remove(id);
+  async removeUser(@Param('id') id: string) {
+    user = await this.usersService.remove(id);
+    return user;
   }
 
   @Patch('/:id')
-  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
-    return this.usersService.update(id, body);
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    user = await this.usersService.update(id, body);
+    return user;
   }
 }
