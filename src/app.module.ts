d6bbdf5bb866b92d3c82e7993ef22fda9c88f667
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-10-09 20:44:14 -0300

    delete unneeded app controller and app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UserModule } from './users/users.module';
 import { ReportModule } from './report/report.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { User } from './users/users.entity';
 import { Report } from './report/report.entity';
 import { AuthService } from './users/auth.service';
@@ -19,7 +17,5 @@ import { AuthService } from './users/auth.service';
     UserModule,
     ReportModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
