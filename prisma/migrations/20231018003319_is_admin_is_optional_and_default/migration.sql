4552791a724b86a2d93a6110f8dfb54243bd2324
Arthur Rabelo <arthurrabelopro@gmail.com>
2023-10-17 21:56:50 -0300

    start migration from typeORM to prisma

---
+++ prisma/migrations/20231018003319_is_admin_is_optional_and_default/migration.sql
@@ -0,0 +1,14 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false
+);
+INSERT INTO "new_users" ("email", "id", "isAdmin", "password") SELECT "email", "id", "isAdmin", "password" FROM "users";
+DROP TABLE "users";
+ALTER TABLE "new_users" RENAME TO "users";
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
